4ecec8cedf9d85cbe1edf0aa565a3fbe2716d333
Aaron Meyer <ameyer@ucla.edu>
2021-05-23 11:52:59 -0700

    Initial code

---
+++ tests/test_model.py
@@ -0,0 +1,94 @@
+import unittest
+import numpy as np
+from scipy.special import binom
+from valentbind.model import polyc, polyfc
+
+
+def genPerm(len, sum):
+    if len <= 1:
+        yield [sum]
+    else:
+        for i in range(sum + 1):
+            for sub in genPerm(len - 1, sum - i):
+                yield sub + [i]
+
+
+def multinomial(params):
+    if len(params) == 1:
+        return 1
+    return binom(sum(params), params[-1]) * multinomial(params[:-1])
+
+
+def polyfc2(L0, KxStar, f, Rtot, LigC, Kav):
+    """ This function should give the same result as polyfc() but less efficient.
+    This function is used for testing only. Use polyfc() for random complexes calculation"""
+    LigC = np.array(LigC)
+    assert LigC.ndim == 1
+    LigC = LigC / np.sum(LigC)
+
+    Cplx = np.array(list(genPerm(LigC.size, f)))
+    Ctheta = np.exp(np.dot(Cplx, np.log(LigC).reshape(-1, 1))).flatten()
+    Ctheta *= np.array([multinomial(Cplx[i, :]) for i in range(Cplx.shape[0])])
+    assert abs(sum(Ctheta) - 1.0) < 1e-12
+
+    return polyc(L0, KxStar, Rtot, Cplx, Ctheta, Kav)
+
+
+class TestPolyc(unittest.TestCase):
+    def test_equivalence(self):
+        L0 = np.random.rand() * 10.0 ** np.random.randint(-15, -5)
+        KxStar = np.random.rand() * 10.0 ** np.random.randint(-15, -5)
+        f = np.random.randint(1, 10)
+        nl = np.random.randint(1, 10)
+        nr = np.random.randint(1, 10)
+        Rtot = np.floor(100.0 + np.random.rand(nr) * (10.0 ** np.random.randint(4, 6, size=nr)))
+        LigC = np.random.rand(nl) * (10.0 ** np.random.randint(1, 2, size=nl))
+        Kav = np.random.rand(nl, nr) * (10.0 ** np.random.randint(3, 7, size=(nl, nr)))
+
+        res = polyfc(L0, KxStar, f, Rtot, LigC, Kav)
+        res2 = polyfc2(L0, KxStar, f, Rtot, LigC, Kav)
+        res20 = np.sum(res2[0])
+        res21 = np.sum(res2[1])
+
+        self.assertTrue(abs(res[0] - res20) < res[0] * 1e-7)
+        self.assertTrue(abs(res[1] - res21) < res[1] * 1e-7)
+        self.assertTrue(abs(np.sum(res[2]) - res[0]) < res[1] * 1e-3)
+
+    def test_null_monomer(self):
+        # [3 0 0] should be equivalent to [3 0 5] if the last ligand has affinity 0
+        L0 = np.random.rand() * 10.0 ** np.random.randint(-15, -5)
+        KxStar = np.random.rand() * 10.0 ** np.random.randint(-15, -5)
+        Rtot = [1e5]
+        Kav = [[2e7], [3e5], [0]]
+
+        res11 = polyc(L0, KxStar, Rtot, [[3, 0, 0]], [1], Kav)
+        res12 = polyc(L0, KxStar, Rtot, [[3, 0, 5]], [1], Kav)
+        res21 = polyc(L0, KxStar, Rtot, [[0, 6, 0]], [1], Kav)
+        res22 = polyc(L0, KxStar, Rtot, [[0, 6, 3]], [1], Kav)
+        res31 = polyc(L0, KxStar, Rtot, [[2, 4, 0]], [1], Kav)
+        res32 = polyc(L0, KxStar, Rtot, [[2, 4, 5]], [1], Kav)
+
+        for i in range(2):
+            self.assertEqual(res11[i], res12[i])
+            self.assertEqual(res21[i], res22[i])
+            self.assertEqual(res31[i], res32[i])
+
+    def test_Lfbnd(self):
+        L0 = np.random.rand() * 10.0 ** np.random.randint(-15, -5)
+        KxStar = np.random.rand() * 10.0 ** np.random.randint(-15, -5)
+        nl = 4
+        nr = np.random.randint(1, 10)
+        Rtot = np.floor(100.0 + np.random.rand(nr) * (10.0 ** np.random.randint(4, 6, size=nr)))
+        Kav = np.random.rand(nl, nr) * (10.0 ** np.random.randint(3, 7, size=(nl, nr)))
+        Cplx = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+        Ctheta = np.random.rand(4)
+        Ctheta = Ctheta / sum(Ctheta)
+
+        res = polyc(L0, KxStar, Rtot, Cplx, Ctheta, Kav)
+        self.assertAlmostEqual(np.sum(res[0]), np.sum(res[2]))
+        for i in range(len(res[0])):
+            self.assertAlmostEqual(res[0][i], np.sum(res[1], axis=1)[i])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ valentbind/model.py
@@ -0,0 +1,138 @@
+"""
+Implementation of a simple multivalent binding model.
+"""
+
+import numpy as np
+import jax.numpy as jnp
+from jax import jit, jacfwd
+from jax.config import config
+from scipy.optimize import root
+from scipy.special import binom
+
+config.update("jax_enable_x64", True)
+
+
+def Req_func(Req, Rtot, L0fA, AKxStar, f):
+    """ Mass balance. Transformation to account for bounds. """
+    Phisum = jnp.dot(AKxStar, Req.T)
+    return Req + L0fA * Req * (1 + Phisum) ** (f - 1) - Rtot
+
+
+Req_func_jit = jit(Req_func)
+Req_func_J_jit = jit(jacfwd(Req_func, 0))
+
+
+def polyfc(L0, KxStar, f, Rtot, LigC, Kav):
+    """
+    The main function. Generate all info for heterogenenous binding case
+    L0: concentration of ligand complexes.
+    KxStar: detailed balance-corrected Kx.
+    f: valency
+    Rtot: numbers of each receptor appearing on the cell.
+    LigC: the composition of the mixture used.
+    Kav: a matrix of Ka values. row = IgG's, col = FcgR's
+    """
+    # Data consistency check
+    Kav = np.array(Kav)
+    Rtot = np.array(Rtot, dtype=float)
+    assert Rtot.ndim <= 1
+    LigC = np.array(LigC)
+    assert LigC.ndim <= 1
+    LigC = LigC / np.sum(LigC)
+    assert LigC.size == Kav.shape[0]
+    assert Rtot.size == Kav.shape[1]
+    assert Kav.ndim == 2
+
+    # Run least squares to get Req
+    Req = Req_Regression(L0, KxStar, f, Rtot, LigC, Kav)
+
+    nr = Rtot.size  # the number of different receptors
+
+    Phi = np.ones((LigC.size, nr + 1)) * LigC.reshape(-1, 1)
+    Phi[:, :nr] *= Kav * Req * KxStar
+    Phisum = np.sum(Phi[:, :nr])
+
+    Lbound = L0 / KxStar * ((1 + Phisum) ** f - 1)
+    Rbound = L0 / KxStar * f * Phisum * (1 + Phisum) ** (f - 1)
+    vieq = L0 / KxStar * binom(f, np.arange(1, f + 1)) * np.power(Phisum, np.arange(1, f + 1))
+
+    return Lbound, Rbound, vieq
+
+
+def Req_Regression(L0, KxStar, f, Rtot, LigC, Kav):
+    """Run least squares regression to calculate the Req vector"""
+    A = np.dot(LigC.T, Kav)
+    L0fA = L0 * f * A
+    AKxStar = A * KxStar
+
+    # Identify an initial guess just on max monovalent interaction
+    x0 = np.max(L0fA, axis=0)
+    x0 = np.multiply(1.0 - np.divide(x0, 1 + x0), Rtot)
+
+    # Solve Req by calling least_squares() and Req_func()
+    lsq = root(Req_func_jit, x0, jac=Req_func_J_jit, method="lm", args=(Rtot, L0fA, AKxStar, f), options={"maxiter": 3000})
+    assert lsq["success"], "Failure in rootfinding. " + str(lsq)
+
+    return lsq["x"].reshape(1, -1)
+
+
+def Req_func2(Req, L0, KxStar, Rtot, Cplx, Ctheta, Kav):
+    Psi = Req * Kav * KxStar
+    Psi = jnp.pad(Psi, ((0, 0), (0, 1)), constant_values=1)
+    Psirs = jnp.sum(Psi, axis=1).reshape(-1, 1)
+    Psinorm = (Psi / Psirs)[:, :-1]
+
+    Rbound = L0 / KxStar * jnp.sum(Ctheta.reshape(-1, 1) * jnp.dot(Cplx, Psinorm) * jnp.exp(jnp.dot(Cplx, jnp.log1p(Psirs - 1))), axis=0)
+    return Req + Rbound - Rtot
+
+
+Req_func2_jit = jit(Req_func2)
+Req_func2_J_jit = jit(jacfwd(Req_func2, 0))
+
+
+def polyc(L0, KxStar, Rtot: np.ndarray, Cplx: np.ndarray, Ctheta: np.ndarray, Kav: np.ndarray):
+    """
+    The main function to be called for multivalent binding
+    :param L0: concentration of ligand complexes
+    :param KxStar: Kx for detailed balance correction
+    :param Rtot: numbers of each receptor on the cell
+    :param Cplx: the monomer ligand composition of each complex
+    :param Ctheta: the composition of complexes
+    :param Kav: Ka for monomer ligand to receptors
+    :return:
+        Lbound: a list of Lbound of each complex
+        Rbound: a list of Rbound of each kind of receptor
+    """
+    # Consistency check
+    Kav = np.array(Kav)
+    assert Kav.ndim == 2
+    Rtot = np.array(Rtot, dtype=float)
+    assert Rtot.ndim == 1
+    Cplx = np.array(Cplx)
+    assert Cplx.ndim == 2
+    Ctheta = np.array(Ctheta)
+    assert Ctheta.ndim == 1
+
+    assert Kav.shape[0] == Cplx.shape[1]
+    assert Kav.shape[1] == Rtot.size
+    assert Cplx.shape[0] == Ctheta.size
+    Ctheta = Ctheta / np.sum(Ctheta)
+
+    # Solve Req
+    lsq = root(Req_func2_jit, Rtot, jac=Req_func2_J_jit, method="lm", args=(L0, KxStar, Rtot, Cplx, Ctheta, Kav), options={"maxiter": 3000})
+    assert lsq["success"], "Failure in rootfinding. " + str(lsq)
+    Req = lsq["x"].reshape(1, -1)
+
+    # Calculate the results
+    Psi = np.ones((Kav.shape[0], Kav.shape[1] + 1))
+    Psi[:, : Kav.shape[1]] *= Req * Kav * KxStar
+    Psirs = np.sum(Psi, axis=1).reshape(-1, 1)
+    Psinorm = (Psi / Psirs)[:, :-1]
+
+    Lbound = L0 / KxStar * Ctheta * np.expm1(np.dot(Cplx, np.log(Psirs))).flatten()
+    Rbound = L0 / KxStar * Ctheta.reshape(-1, 1) * np.dot(Cplx, Psinorm) * np.exp(np.dot(Cplx, np.log(Psirs)))
+    Lfbnd = L0 / KxStar * Ctheta * np.exp(np.dot(Cplx, np.log(Psirs - 1.0))).flatten()
+    assert len(Lbound) == len(Ctheta)
+    assert Rbound.shape[0] == len(Ctheta)
+    assert Rbound.shape[1] == len(Rtot)
+    return Lbound, Rbound, Lfbnd
